cb09712a10f429486278d82c2d0c89ba574d0dd4
Egor Grekhnev <egorgrekhnev@mail.ru>
2018-10-21 16:30:06 +0300

    catalog list

--- serv.js
+++ serv.js
@@ -6,10 +6,44 @@ var app = express();
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
+app.set('view engine', 'ejs');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+var lot_1 = {
+  name: "YEEZY BOOST 350 V2 'ZEBRA'",
+  url: 'https://raw.githubusercontent.com/Egor14/web/master/yeezy_zebra.png',
+  price : 200,
+  lotID: 1,
+  time: "00:50"
+};
+
+var lot_2 = {
+  name: "NIKE AIR JORDAN",
+  url: 'https://raw.githubusercontent.com/Egor14/web/master/jordan.png',
+  price : 100,
+  lotID: 2,
+  time: "00:50"
+};
+
+var lot_3 = {
+  name: "NIKE AIR MAX",
+  url: 'https://raw.githubusercontent.com/Egor14/web/master/air.png',
+  price : 70,
+  lotID: 3,
+  time: "00:50"
+};
+
+var catalog = [lot_1, lot_2, lot_3];
+
+var catalogJSON = JSON.stringify(catalog);
+
+console.log(catalog);
+
+
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/index.html');
+	res.render('index', {catalogJSON : catalogJSON});
 });
 
 app.get('/buy', function(req, res) {
@@ -24,6 +58,8 @@ app.post('/filter', urlencodedParser, function (req, res) {
 	console.log(req.body);
 	res.redirect('/');
 })
+
+
 
 
 app.listen(3000);
